a8fae40e55beef674039b3d323c0d379db66d6af
martinyeo23 <martinyeo23@gmail.com>
2025-11-26 23:23:06 +0800

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,3 +1,17 @@
+#Answer
+
+def find_and_replace(lst, find_val, replace_val):
+    
+    for i in range(len(lst)):
+        if lst[i] == find_val:
+              lst[i]=replace_val
+    return lst
+
+find_and_replace([1, 2, 3, 4, 2, 2], 2, 5)
+find_and_replace(["apple", "banana", "apple"], "apple", "orange")
+
+
+#Question
 def find_and_replace(lst, find_val, replace_val):
     """
     Task 1
