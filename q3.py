88ad7746caa1e165e1ae5d84983c09b5082e349b
martinyeo23 <martinyeo23@gmail.com>
2026-03-09 14:04:22 +0800

    Update q3.py

--- q3.py
+++ q3.py
@@ -5,10 +5,23 @@ def update_dictionary(dct, key, value):
     - If the key already exists in dct, print the original value, then update its value.
     - Return the updated dictionary.
     """
-    return
+    # Check if key already exists
+    if key in dct:
+        print("Original value:", dct[key])
+
+    # Update or add the key-value pair
+    dct[key] = value
+    return dct
 
 
 # Task 2
 # Invoke the function "update_dictionary" using the following scenarios:
 # - {}, "name", "Alice"
 # - {"age": 25}, "age", 26
+
+update_dictionary({}, "name", "Alice")
+# returns {'name': 'Alice'}
+update_dictionary({"age": 25}, "age", 26)
+# returns 
+Original value: 25
+{'age': 26}
