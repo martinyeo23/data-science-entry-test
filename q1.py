0c5920a234a3338a3a467ec629fbfccab2dd7ca0
martinyeo23 <martinyeo23@gmail.com>
2026-03-09 13:19:06 +0800

    Update q1.py

--- q1.py
+++ q1.py
@@ -6,10 +6,28 @@ def swap(x, y):
     - Return -1 if x and y is not numeric, and
     - print the swapped values if both x and y are numeric.
     """
-    return
+
+#to test if variables are non numeric
+    if not isinstance(x,(int, float, complex)) or not isinstance(y,(int, float, complex)):
+        return -1 
+    
+#to test if variables are both numeric
+    elif isinstance(x,(int, float, complex)) and isinstance(y,(int, float, complex)):
+
+        #Swapping values without creating new variables, by using arithmetic
+        x = x + y # x is now total of both numbers
+        y = x - y # total less y assigns y with the original x value
+        x = x - y #  total less original x value will
+
+        print("Variables with swapped values")
+        print("x =", x)
+        print("y =", y)
 
 
 # Task 2
 # Invoke the function "swap" using the following scenarios:
 # - "Apple", 10
 # - 9, 17
+
+swap("Apple",-3)
+swap(9,-17)
